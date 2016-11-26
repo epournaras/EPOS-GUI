9057133cc8a79ff131c9f4fb4b316da28d095287
MelikaAyoughi <melikaayoughi@gmail.com>
2016-11-26 10:40:18 +0330

    changing the main to open report window - it's commented for now - you can uncomment showReportWindow and comment showConfigurationWindow for now to see. but nothing special on report window

--- src/controller/MainApplication.java
+++ src/controller/MainApplication.java
@@ -19,13 +19,14 @@ public class MainApplication extends Application {
 	
 	private Stage primaryStage;
 	private AnchorPane configurationWindow;
+	private AnchorPane reportWindow;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Configuration");
-
         showConfigurationWindow();
+//        showReportWindow();
 	}
 	
 	/**
@@ -41,11 +42,30 @@ public class MainApplication extends Application {
             // Show the scene containing the configuration window.
             Scene scene = new Scene(configurationWindow);
             primaryStage.setScene(scene);
-            primaryStage.show();
-            
+            primaryStage.show();      
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    /**
+     * Shows the report window
+     */
+    private void showReportWindow(){
+    	try {
+    		// Load configuration from fxml file.
+            FXMLLoader reportWindowloader = new FXMLLoader();
+            reportWindowloader.setLocation(MainApplication.class.getResource("view/reportWindow.fxml"));
+            reportWindow = (AnchorPane) reportWindowloader.load();
+            
+            // Show the scene containing the configuration window.
+            Scene scene = new Scene(reportWindow);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
     
 	public static void main(String[] args) {
